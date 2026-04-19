895926f59a93c47196bb66416b6735cc7a8edb1e
Lucas Monzón <92001159+lcmonzon@users.noreply.github.com>
2026-04-19 15:40:18 -0300

    Cambios hechos
    Se quito la base de angular y se genero un footer

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
